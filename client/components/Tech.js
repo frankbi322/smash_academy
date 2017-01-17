eb82b895ed7bfcf26f956484fb28441a81039aa9
Frank Bi <frankbi322@gmail.com>
2017-01-16 16:17:16 -0800

    changes port to either env port or localhost 3000

--- client/components/Tech.js
+++ client/components/Tech.js
@@ -13,7 +13,7 @@ const Tech = React.createClass({
         </Link>
         </div>
           <figcaption>
-            <p class="centered">{tech.tech}</p>
+            <p className="centered">{tech.tech}</p>
           </figcaption>
 
       </figure>

46a68a4510779884f8891ee0fe2572c9af20623c
Frank Bi <frankbi322@gmail.com>
2017-01-17 11:24:42 -0800

    fixed

--- client/reducers/rootReducer.js
+++ client/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import techs from './techs';
+import techs from './techsReducer';
 
 const rootReducer = combineReducers({techs, routing: routerReducer});
 
